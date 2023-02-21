02676de55e10d463f4f3ef2a947f71f9237c6956
Niklas Baudy <niklas.baudy@vanniktech.de>
2023-02-21 13:01:21 +0100

    Prepare fork for publishing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 buildscript {
 
     extra["minSdkVersion"] = 18
-    extra["compileSdkVersion"] = 31
-    extra["targetSdkVersion"] = 31
+    extra["compileSdkVersion"] = 33
+    extra["targetSdkVersion"] = 33
     
     repositories {
         google()
@@ -11,9 +11,8 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
-        classpath("com.android.tools.build:gradle:7.0.2")
-        classpath("io.deepmedia.tools:publisher:0.6.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
+        classpath("com.android.tools.build:gradle:7.4.1")
     }
 }
 
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,12 +1,7 @@
-import io.deepmedia.tools.publisher.common.GithubScm
-import io.deepmedia.tools.publisher.common.License
-import io.deepmedia.tools.publisher.common.Release
-import io.deepmedia.tools.publisher.sonatype.Sonatype
-
 plugins {
     id("com.android.library")
     id("kotlin-android")
-    id("io.deepmedia.tools.publisher")
+    id("com.vanniktech.maven.publish") version "0.24.0"
 }
 
 android {
@@ -28,35 +23,4 @@ dependencies {
     androidTestImplementation("androidx.test:rules:1.4.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("org.mockito:mockito-android:2.28.2")
-}
-
-publisher {
-    project.description = "Accelerated video transcoding using Android MediaCodec API without native code (no LGPL/patent issues)."
-    project.artifact = "transcoder"
-    project.group = "com.otaliastudios"
-    project.url = "https://github.com/natario1/Transcoder"
-    project.scm = GithubScm("natario1", "Transcoder")
-    project.addLicense(License.APACHE_2_0)
-    project.addDeveloper("natario1", "mat.iavarone@gmail.com")
-    release.sources = Release.SOURCES_AUTO
-    release.docs = Release.DOCS_AUTO
-    release.version = "0.10.5"
-
-    directory()
-
-    sonatype {
-        auth.user = "SONATYPE_USER"
-        auth.password = "SONATYPE_PASSWORD"
-        signing.key = "SIGNING_KEY"
-        signing.password = "SIGNING_PASSWORD"
-    }
-
-    sonatype("snapshot") {
-        repository = Sonatype.OSSRH_SNAPSHOT_1
-        release.version = "latest-SNAPSHOT"
-        auth.user = "SONATYPE_USER"
-        auth.password = "SONATYPE_PASSWORD"
-        signing.key = "SIGNING_KEY"
-        signing.password = "SIGNING_PASSWORD"
-    }
 }
